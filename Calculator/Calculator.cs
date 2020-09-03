bfa0aae2875e60ff9372daa2214aabb336629858
Lasse Andresen <201810604@post.au.dk>
2020-09-03 14:01:45 +0200

    Added Calculator class

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main()
+        {
+            Calculator cal = new Calculator();
+
+            Console.WriteLine("{0}",cal.Power(2, 3));
+            Console.WriteLine("{0}", cal.Power(-2, 3));
+            Console.WriteLine("{0}", cal.Power(2, -3));
+            Console.WriteLine("{0}", cal.Power(-2, -3));
+        }
+        public double Add(double a, double b)
+        {
+            return a+b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a-b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a*b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+    }
+}
